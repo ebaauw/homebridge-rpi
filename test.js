a66cfaddfa56f5d98fb43da22aa8084403dfe5c4
ebaauw <erik.baauw@xs4all.nl>
2020-05-24 23:24:25 +0200

    Update test.js

--- test.js
+++ test.js
@@ -19,49 +19,49 @@ class Test {
     })
   }
 
-  async colourLoop (led = 0) {
-    let d
+  async colourLoop (bri = 1) {
+    // let d
     let r = 0
     let g = 1
     let b = 255
     while (true) {
       if (r < 255 && g === 0 && b === 255) {
-        if (r === 0) {
-          const now = new Date()
-          if (d != null) {
-            console.log('cycle took %d ms', now - d)
-          }
-          d = now
-          console.log('red in')
-        }
+        // if (r === 0) {
+        //   const now = new Date()
+        //   if (d != null) {
+        //     console.log('cycle took %d ms', now - d)
+        //   }
+        //   d = now
+        //   console.log('red in')
+        // }
         r++
       } else if (r === 255 && g === 0 && b > 0) {
-        if (b === 255) {
-          console.log('blue out')
-        }
+        // if (b === 255) {
+        //   console.log('blue out')
+        // }
         b--
       } else if (r === 255 && g < 255 && b === 0) {
-        if (g === 0) {
-          console.log('green in')
-        }
+        // if (g === 0) {
+        //   console.log('green in')
+        // }
         g++
       } else if (r > 0 && g === 255 && b === 0) {
-        if (r === 255) {
-          console.log('red out')
-        }
+        // if (r === 255) {
+        //   console.log('red out')
+        // }
         r--
       } else if (r === 0 && g === 255 && b < 255) {
-        if (b === 0) {
-          console.log('blue in')
-        }
+        // if (b === 0) {
+        //   console.log('blue in')
+        // }
         b++
       } else if (r === 0 && g > 0 && b === 255) {
-        if (g === 255) {
-          console.log('green out')
-        }
+        // if (g === 255) {
+        //   console.log('green out')
+        // }
         g--
       }
-      this.blinkt.setLed(led, 1, r, g, b)
+      this.blinkt.setAllLeds(bri, r, g, b)
       await this.blinkt.update()
       if (this.interrupted) {
         return
@@ -69,19 +69,35 @@ class Test {
     }
   }
 
-  async cylon (r = 255, g = 0, b = 0) {
-    let led = 0
-    let left = false
-    this.blinkt.setLed(0, 1, r, g, b)
+  // Inspired by: https://github.com/pimoroni/blinkt/blob/master/examples/larson.py
+  async cylon (bri = 1) {
+    const values = [0, 0, 0, 0, 0, 0, 0, 16, 64, 255, 64, 16, 0, 0, 0, 0, 0, 0, 0]
+    const delays = [
+      1 + Math.sin(0.5 * Math.PI),
+      1 + Math.sin(0.4 * Math.PI),
+      1 + Math.sin(0.3 * Math.PI),
+      1 + Math.sin(0.2 * Math.PI),
+      1 + Math.sin(0.1 * Math.PI),
+      1 + Math.sin(0.0 * Math.PI),
+      1 + Math.sin(0.1 * Math.PI),
+      1 + Math.sin(0.2 * Math.PI),
+      1 + Math.sin(0.3 * Math.PI),
+      1 + Math.sin(0.4 * Math.PI),
+      1 + Math.sin(0.5 * Math.PI)
+    ]
+    let offset = 11
+    let up = true
     while (true) {
+      for (let i = 0; i <= 7; i++) {
+        this.blinkt.setLed(i, bri, values[offset + i], 0, 0)
+      }
       await this.blinkt.update()
-      await delay()
-      this.blinkt.shiftLeds(left)
-      led += left ? -1 : 1
-      if (led === 7) {
-        left = true
-      } else if (led === 0) {
-        left = false
+      await delay(40 * delays[offset])
+      offset += up ? -1 : 1
+      if (offset === 11) {
+        up = true
+      } else if (offset === 0) {
+        up = false
       }
       if (this.interrupted) {
         return
@@ -92,7 +108,8 @@ class Test {
   async main () {
     try {
       await this.blinkt.init()
-      // await this.colourLoop(7)
+      // await this.colourLoop()
+      // this.interrupted = false
       await this.cylon()
       console.log('Exiting...')
       await this.blinkt.disconnect()
